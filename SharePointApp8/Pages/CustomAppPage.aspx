3bd29266d7a78a01c80e43cd10b30ce63311aa4a
David Mutua <mutuadavi@gmail.com>
2017-09-30 13:08:39 +0100

    Implemented Search Module

--- SharePointApp8/Pages/CustomAppPage.aspx
+++ SharePointApp8/Pages/CustomAppPage.aspx
@@ -5,8 +5,11 @@
 
 <asp:Content ContentPlaceHolderId="PlaceHolderAdditionalPageHead" runat="server">
     <script type="text/javascript" src="../Scripts/jquery-1.9.1.min.js"></script>
-    <SharePoint:ScriptLink name="sp.js" runat="server" OnDemand="true" LoadAfterUI="true" Localizable="false" />
+    <script type="text/javascript" src="/_layouts/15/sp.runtime.js"></script>
+    <script type="text/javascript" src="/_layouts/15/sp.js"></script>
+    <script type="text/javascript" src="/_layouts/15/SP.Taxonomy.js"></script>
     <script type="text/javascript" src="/_layouts/15/SP.UserProfiles.js"></script>
+    <script type="text/javascript" src="/_layouts/15/SP.Search.js"></script>
 
     <%--Custom JS Scripts--%>
     <script type="text/javascript" src="../Scripts/AppCustom.js"></script>
@@ -24,8 +27,10 @@
         
         <p><button role="button" id="createLibrary">createDocLibrary</button></p>
         <p><button role="button" id="userPermiSions">User Permissions</button></p>
+        <p><button role="button" id="getProfileProperties">User Profile</button></p>
+        <p><button role="button" id="searchFunc">Search</button></p>
         
-
+        
         <p>
             <input type="text" id="listToDelete" /><button role="button" id="DeleteList">Delete List</button>
         </p>
--- SharePointApp8/pkg/Debug/SharePointApp8/SharePointApp8_Feature1/Scripts/UserPermissions.js
+++ SharePointApp8/pkg/Debug/SharePointApp8/SharePointApp8_Feature1/Scripts/UserPermissions.js
@@ -2,6 +2,9 @@
 
 $(function () {
     $('#userPermiSions').click(userPermiSions);
+    $('#getProfileProperties').click(getProfileProperties);
+    $('#searchFunc').click(searchFunc);
+    
 });
 
 // User Permissions:
@@ -39,3 +42,76 @@ function userPermiSions(event) {
     }
 
 } // userPermiSions()
+
+
+// User Profiles
+// NB: User Profiles are Stored outside the App. Thus configure AppManifest for Read Perm
+function getProfileProperties(evnt) {
+    evnt.preventDefault();
+
+    var context = SP.ClientContext.get_current();
+    var manager = new SP.UserProfiles.PeopleManager(context);
+    var profile = manager.getMyProperties();
+
+    context.load(profile, "DisplayName", "UserProfileProperties");
+    context.executeQueryAsync(success, fail);
+
+    function success() {
+        var message = $('#message');
+        message.text("User Profile Properties for " + profile.get_displayName());
+        message.append("<br />");
+
+        // iterate thro profile returning only non-null properties
+        var props = profile.get_userProfileProperties();
+        for (var key in props) {
+            var value = props[key];
+            if (value.length > 0) {
+                message.append(key + " : " + value);
+                message.append("<br />");
+            }
+        }
+    }
+
+    function fail(sender, args) {
+        alert("Call failed in getProfileProperties(). Error: " + args.get_message());
+    }
+
+} // getProfileProperties()
+
+
+// Search
+// Perform a Keyword Query Search
+function searchFunc(event) {
+    event.preventDefault();
+
+    var context = SP.ClientContext.get_current();
+
+    try {
+        var queryText = "boxes";
+        var query = new Microsoft.SharePoint.Client.Search.Query.KeywordQuery(context);
+        query.set_queryText(queryText);
+
+        var exec = new Microsoft.SharePoint.Client.Search.Query.SearchExecutor(context);
+        var results = exec.executeQuery(query);
+
+        context.executeQueryAsync(success, fail);
+    } catch (Exception) {
+        alert(Exception.message);
+    }
+
+    function success() {
+        var message = $('#message');
+        message.text("Search Results for \"" + queryText + "\"");
+        message.append("<br />");
+        
+        var rows = results.m_value.ResultTables[0].ResultRows;
+        $.each(rows, function (index, value) {
+            message.append(value.Title + ": " + value.Path);
+            message.append("<br />");
+        });
+    }
+
+    function fail(sender, args) {
+        alert("Call failed in searchFunc(). Error: " + args.get_message());
+    }
+} // searchFunc()
